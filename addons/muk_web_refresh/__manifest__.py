9584467f43996033215eeb5c3d155d06ff0761e3
nghia562001 <nghia562001@gmail.com>
2025-09-17 11:47:30 +0700

    Add Theme

---
+++ addons/muk_web_refresh/__manifest__.py
@@ -0,0 +1,41 @@
+{
+    'name': 'MuK Web Refresh', 
+    'summary': 'Automatically refresh any list or kanban view',
+    'description': '''
+        Activate the auto refresh button to reload the view every
+        30 seconds. The refresh will reload and update the data
+        of the view.
+    ''',
+    'version': '19.0.1.0.0',
+    'category': 'Tools/UI',
+    'license': 'LGPL-3', 
+    'author': 'MuK IT',
+    'website': 'http://www.mukit.at',
+    'live_test_url': 'https://my.mukit.at/r/f6m',
+    'contributors': [
+        'Mathias Markl <mathias.markl@mukit.at>',
+    ],
+    'depends': [
+        'web',
+    ],
+    'assets': {
+        'web.assets_backend': [            
+            (
+                'after',
+                '/web/static/src/search/control_panel/control_panel.js',
+                '/muk_web_refresh/static/src/search/control_panel.js',
+            ),            
+            (
+                'after',
+                '/web/static/src/search/control_panel/control_panel.xml',
+                '/muk_web_refresh/static/src/search/control_panel.xml',
+            ),
+        ],
+    },
+    'images': [
+        'static/description/banner.png',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
